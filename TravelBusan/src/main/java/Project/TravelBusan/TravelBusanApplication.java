9ddeb75934e0658a09970f72fdc31bdb79464d90
pv104 <71826117+pv104@users.noreply.github.com>
2023-09-14 22:41:14 +0900

    Add files via upload

---
+++ TravelBusan/src/main/java/Project/TravelBusan/TravelBusanApplication.java
@@ -0,0 +1,13 @@
+package Project.TravelBusan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBusanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBusanApplication.class, args);
+	}
+
+}
---
+++ TravelBusan/src/test/java/Project/TravelBusan/TravelBusanApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.TravelBusan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBusanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
